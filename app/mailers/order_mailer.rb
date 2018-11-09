ef1eb664fefb5d02e0cb0dbf70613972fa18bec9
Zoe Gelech <zoe.gelech@gmail.com>
2018-11-09 03:39:44 +0000

    trying to get user into email

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,10 +1,11 @@
 class OrderMailer < ApplicationMailer
   default from: "no-reply@jungle.com"
 
-    def order_email(order)
+    def order_email(user, order)
+    @user = user
     @order = order
     @url  = 'http://0.0.0.0:3000/orders/'
-    mail(to: 'test@example.com', subject: 'order:  has Been Made!')
+    mail(to: @user.email, subject: 'order:  has Been Made!')
     end
   end
 
