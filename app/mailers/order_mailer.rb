1fdbe379cea49bd66c9cc95b48a94159c9380b9e
Zoe Gelech <zoe.gelech@gmail.com>
2018-11-09 02:31:20 +0000

    have mailer set up code implemented

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,2 +1,9 @@
 class OrderMailer < ApplicationMailer
+  default from: "no-reply@jungle.com"
+
+  @user = params[:user]
+  @url  = 'http://example.com/orders/'
+  mail(to: current_user.email, subject: 'Your order had Been Made!')
+  end
 end
+
